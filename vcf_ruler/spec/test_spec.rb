471fd6727de6f6ff0c2cab99458235c9e6daec01
jeremypumphrey <jeremypumphrey@gmail.com>
2015-06-11 15:33:23 -0500

    adding test rspec

---
+++ vcf_ruler/spec/test_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe 'My behaviour' do
+
+  it 'should pass a true' do
+    true.should == true
+  end
+
+  it 'should pass a true' do
+    false.should == false
+  end
+
+  # it 'should fail a false' do
+  #   true.should == false
+  # end
+end
