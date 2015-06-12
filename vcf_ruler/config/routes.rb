43328421fbc83b43f10c15ddeeed47e6dec6dfd9
josephverbeck <joseph.verbeck@nih.gov>
2015-06-12 15:06:03 -0400

    Missed the router file

--- vcf_ruler/config/routes.rb
+++ vcf_ruler/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'upload' => 'file_upload#upload'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
