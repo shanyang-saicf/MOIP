d9dc6b0523b16a3b0672df19ed2f47846c70fd70
josephverbeck <joseph.verbeck@nih.gov>
2015-06-15 09:11:15 -0400

    upload view and route....still a work in progress

--- vcf_ruler/config/routes.rb
+++ vcf_ruler/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'upload' => 'file_upload#upload'
 
+  post 'upload' => 'file_upload#upload'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
