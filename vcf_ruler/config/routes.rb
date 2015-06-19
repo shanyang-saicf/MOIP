b828531c63e65f8daee27af2559f888b55db9cda
jeremypumphrey <jeremypumphrey@gmail.com>
2015-06-19 09:02:48 -0500

    or not, removing root

--- vcf_ruler/config/routes.rb
+++ vcf_ruler/config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'image#new'
+#root 'image#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
