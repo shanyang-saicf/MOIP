f62a64090487a0622eeada408b05677a0314b314
josephverbeck <joseph.verbeck@nih.gov>
2015-07-07 09:37:49 -0400

    Adding a return of a json object

--- vcf_ruler/app/controllers/file_upload_controller.rb
+++ vcf_ruler/app/controllers/file_upload_controller.rb
@@ -18,7 +18,7 @@ class FileUploadController < ApplicationController
     @fileupload = FileUpload.new( user_params )
     #@fileupload.save
     @file = Paperclip.io_adapters.for(@fileupload.fileupload)
-    hashMap = {}
+    hashJson = []
     begin
       keyCount = 0
       File.open(@file.path).each do |line|
@@ -28,7 +28,19 @@ class FileUploadController < ApplicationController
           #p "ID " + metaClass.id
         end
         if !line.match("OMAPALT=A").nil?
-          p line
+          data = line.split("\t")
+          hashMap = {'CHROM' => '',	'POS' => '',	'ID' => '',	'REF' => '',	'ALT' => '', 'QUAL' => '',	'FILTER' => '',	'INFO' => '',	'FORMAT' => '',	'Mock_rep1_DNA' => ''}
+          hashMap['CHROM'] = data[0]
+          hashMap['POS'] = data[1]
+          hashMap['ID'] = data[2]
+          hashMap['REF'] = data[3]
+          hashMap['ALT'] = data[4]
+          hashMap['QUAL'] = data[5]
+          hashMap['FILTER'] = data[6]
+          hashMap['INFO'] = data[7]
+          hashMap['FORMAT'] = data[8]
+          hashMap['Mock_rep1_DNA'] = data[9]
+          hashJson << hashMap
         end
         # count = 0
         # line.split("\t").each do | word |
@@ -40,9 +52,9 @@ class FileUploadController < ApplicationController
         #p hashMap
       end
     end
+    render :json => hashJson.to_json
     #fileData = File.open(@file.path).read
     #p fileData
-    render nothing: true
   end
 
 
