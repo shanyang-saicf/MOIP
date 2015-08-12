addbed3d15707c7bdfb77465c9f75be8c882bd39
josephverbeck <joseph.verbeck@nih.gov>
2015-08-12 13:48:48 -0400

    fixing info_parser so that we get a hash back

--- vcf_ruler/app/models/parser/info_parser.rb
+++ vcf_ruler/app/models/parser/info_parser.rb
@@ -1,3 +1,25 @@
 class InfoParser < Parser
 
+  attr_accessor :info
+
+  def parse(line)
+    infoHash = {}
+    infoArray = line.split(";")
+    infoArray.each do | infoLine |
+      if !infoLine.include? "FUNC"
+        data = infoLine.split("=")
+        infoHash.store(data[0], data[1])
+      else
+        data = infoLine.split("=[")
+        json = ActiveSupport::JSON.decode(("[" + data[1]).tr('\'', '"'))
+        funcHash = {}
+        json[0].each { |key, value|
+          funcHash.store(key, value)
+        }
+        infoHash.store(data[0], funcHash)
+      end
+    end
+    return infoHash
+  end
+
 end
