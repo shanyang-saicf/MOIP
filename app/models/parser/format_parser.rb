2db72d90ea414f0407e5b52bf94e04c177b64360
josephverbeck <joseph.verbeck@nih.gov>
2015-08-19 11:09:03 -0400

    Format are actually keys and Mock_rep1 is it's data...starting to make these into a hash

--- app/models/parser/format_parser.rb
+++ app/models/parser/format_parser.rb
@@ -1,3 +1,20 @@
 class FormatParser < VcfParser
 
+  attr_accessor :format
+
+  def initialize
+    @format = []
+  end
+
+  def parse(line)
+    if line.include? ":"
+      data = line.split(":")
+      data.each do | key |
+        @format << key
+      end
+      return data
+    end
+    return line
+  end
+
 end
--- app/models/parser/mock_rep1_dna_parser.rb
+++ app/models/parser/mock_rep1_dna_parser.rb
@@ -2,7 +2,18 @@ class MockRep1DnaParser < VcfParser
 
   attr_accessor :repDna
 
+  def initialize
+    @repDna = []
+  end
+
   def parse(line)
+    if line.include? ":"
+      data = line.split(":").squish
+      data.each do | formatData |
+        @repDna << formatData
+      end
+      return data
+    end
     return line.squish
   end
 
