e88339acdebcc1de83f51073f0da7b27ea5d46de
jeremypumphrey <jeremypumphrey@gmail.com>
2015-08-20 15:18:51 -0500

    broke the rspec files with default

---
+++ spec/routing/routes_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "routing to rule", :type => :routing do
+  it "routes /rule/index to rule#index" do
+    expect(:get => "/rule/index").to route_to(
+                                              :controller => "rule",
+                                              :action => "index"
+                                          )
+  end
+
+  it "does not expose a list of profiles" do
+    expect(:get => "/profiles").not_to be_routable
+  end
+end
