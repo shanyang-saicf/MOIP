c6ba459b2631a83260db2257ef262f63fd649ff3
jeremypumphrey <jeremypumphrey@gmail.com>
2015-08-20 15:41:30 -0500

    Added message to turn back on CodeClimate reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@
 require "codeclimate-test-reporter"
 #require "rails/all"
 require "rails_helper"
-# CodeClimate::TestReporter.start
+CodeClimate::TestReporter.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
