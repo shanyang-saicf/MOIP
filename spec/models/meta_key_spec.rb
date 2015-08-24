ceb5e038454b40da8245956a6836da1e79b44e44
jeremypumphrey <jeremypumphrey@gmail.com>
2015-08-24 14:52:48 -0500

    adding meta_key tests

--- spec/models/meta_key_spec.rb
+++ spec/models/meta_key_spec.rb
@@ -4,16 +4,42 @@ RSpec.describe "Metakey::MetaKey", type: :model do
 
   it "meta_key_module, identifies a key" do
     line = "##fileformat=VCFv4.1"
-    MetaKeyFactory.is_key(line)
     expect(MetaKeyFactory.is_key(line)).to eq(TRUE)
-
   end
 
-  it "meta_key, sets the class id" do
+  it "meta_key_module, identifies not a key" do
+    line = "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	Mock-rep1-DNA"
+    expect(MetaKeyFactory.is_key(line)).to eq(FALSE)
+  end
+
+  it "meta_key_module, identifies a header" do
+    line = "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	Mock-rep1-DNA"
+    expect(MetaKeyFactory.is_header(line)).to eq(TRUE)
+  end
+
+  it "meta_key_module, identifies not a header" do
+    line = "##fileformat=VCFv4.1"
+    expect(MetaKeyFactory.is_header(line)).to eq(FALSE)
+  end
+
+  it "meta_key_module, line_formatter" do
+    # TODO
+    # line = "##fileformat=VCFv4.1"
+    # expect(MetaKeyFactory.is_header(line)).to eq(FALSE)
+  end
+
+  it "meta_key, sets the class id of unknown" do
     line = "##fileformat=VCFv4.1"
     @metaClass = MetaKeyFactory.new
     @class = @metaClass.findModel(line)
     expect(@class.id).to eq("fileformat")
+  end
+
+  it "meta_key, sets the class id of known" do
+    line = '##FORMAT=<ID=CN,Number=1,Type=Float,Description="Copy number genotype for imprecise events">'
+    @metaClass = MetaKeyFactory.new
+    @class = @metaClass.findModel(line)
+    expect(@class.id).to eq("FORMAT")
   end
 
 end
