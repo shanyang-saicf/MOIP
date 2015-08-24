de93032be4e4dac2193c3e14844b8bd18d845f22
josephverbeck <joseph.verbeck@nih.gov>
2015-08-24 10:52:13 -0400

    Fixing rule upload so it saves to DB and uses file name as id

--- app/controllers/file_upload_controller.rb
+++ app/controllers/file_upload_controller.rb
@@ -16,13 +16,9 @@ class FileUploadController < ApplicationController
   end
 
   def new_rule
-    # params[:file_upload].delete
-    json = ActiveSupport::JSON.encode(params)
-    json = ActiveSupport::JSON.decode(json)
-    json.delete('file_upload')
-    json.delete('controller')
-    json.delete('action')
-    @ruleupload = RuleUpload.new
+    @ruleupload = RuleUpload.new(params.require(:ruleupload).permit(:ruleupload))
+    json = JSON.parse(Paperclip.io_adapters.for(@ruleupload.ruleupload).read)
+    @ruleupload.write_attribute(:_id, File.basename(@ruleupload.ruleupload.original_filename, File.extname(@ruleupload.ruleupload.original_filename)))
     @ruleupload.write_attribute(:rule, json)
     @ruleupload.write_attribute(:sentence, RuleParser.new.parseJson(json))
     @ruleupload.save
@@ -34,7 +30,7 @@ class FileUploadController < ApplicationController
     @file = Paperclip.io_adapters.for(@fileupload.fileupload)
     hashJson = VcfParser.new.fileParse(@file)
     @expression = Expression.new(hashJson)
-    expressionArray = Interpreter.new.interpret(["SVTYPE == CNV","FILTER == PASS", "CN >= 7", {"and"=>["ID != ", "ID != ."]}])
+    expressionArray = Interpreter.new.interpret(RuleUpload.find("CNV").sentence)
     newHash = []
     expressionArray.each do | expression |
        newHash = @expression.testEval(expression)
--- app/models/rule_upload.rb
+++ app/models/rule_upload.rb
@@ -1,7 +1,14 @@
 class RuleUpload
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
+  include Mongoid::Paperclip
 
+  has_mongoid_attached_file :ruleupload
+
+  # do_not_validate_attachment_file_type :ruleupload
+  validates_attachment :ruleupload, content_type: { content_type: ['application/json'] }
+
+  field :_id, :type => String
   field :rule, :type => Hash
   field :sentence, :type => Array
 
