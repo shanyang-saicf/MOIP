d35f2b28df6504787a1b6f888546705f1dfaa4fb
jeremypumphrey <jeremypumphrey@gmail.com>
2015-08-24 10:55:41 -0500

    added rspec tests to header_key

--- spec/models/headerkey/header_key_spec.rb
+++ spec/models/headerkey/header_key_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Headerkey::HeaderKey", type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "parses the header correctly" do
+    line = "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	Mock-rep1-DNA"
+    headers = HeaderKey.new
+    headers.fillHeader(line)
+    expect(headers.headers.length).to eq(10)
+    expect(headers.headers[0]).to eq("CHROM")
+    expect(headers.headers[9]).to eq("Mock-rep1-DNA")
+  end
 end
