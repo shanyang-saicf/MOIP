e3a57dc685125d0744876b0b720cef0dc1931b3c
josephverbeck <joseph.verbeck@nih.gov>
2015-08-25 10:00:14 -0400

    First model rspec test

--- spec/models/rule_interpreter/and_spec.rb
+++ spec/models/rule_interpreter/and_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "RuleInterpreter::And", type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should return true" do
+    And(true, true).evaluate("tester").should == true
+  end
+
 end
