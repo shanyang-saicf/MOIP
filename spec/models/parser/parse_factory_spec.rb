71178c79496ca8a2355f1edc50b6ae8749d92429
jeremypumphrey <jeremypumphrey@gmail.com>
2015-08-25 10:31:49 -0500

    adding broken test in parse_factory

--- spec/models/parser/parse_factory_spec.rb
+++ spec/models/parser/parse_factory_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe "Parser::ParseFactory", type: :model do
     expect(line["ID"]).to eq("MCH4")
   end
 
+  # it "parse_factory, Parse IDs" do
+  #   line = {}
+  #   line["ID"] = parseFactory("ID", "COSM1686998;COSM20417")
+  #   expect(line["ID"][1]).to eq("COSM20417")
+  # end
+
   it "parse_factory, Parse REF" do
     line = {}
     line["REF"] = parseFactory("REF", "G")
@@ -31,6 +37,12 @@ RSpec.describe "Parser::ParseFactory", type: :model do
     line = {}
     line["ALT"] = parseFactory("ALT", "G")
     expect(line["ALT"]).to eq("G")
+  end
+
+  it "parse_factory, Parse ALT with ," do
+    line = {}
+    line["ALT"] = parseFactory("ALT", "C,G,T")
+    expect(line["ALT"][2]).to eq("T")
   end
 
   it "parse_factory, Parse QUAL" do
@@ -45,7 +57,7 @@ RSpec.describe "Parser::ParseFactory", type: :model do
     expect(line["FILTER"]).to eq("PASS")
   end
 
-  it "parse_factory, Parse INFO wihtout FUNC" do
+  it "parse_factory, Parse INFO without FUNC" do
     line = {}
     line["INFO"] = parseFactory("INFO", 'PRECISE=FALSE;SVTYPE=CNV;END=65312407;LEN=54127868;NUMTILES=26;CDF_MAPD=0.025000:1.811614,0.050000:1.825542,0.100000:1.841732,0.200000:1.861530,0.250000:1.869107,0.500000:1.900000,0.750000:1.931404,0.800000:1.939265,0.900000:1.960111,0.950000:1.977495,0.975000:1.992698;CDF_LD=0.025000:1.851119,0.050000:1.854235,0.100000:1.860468,0.200000:1.872932,0.250000:1.879164,0.500000:1.910325,0.750000:1.941485,0.800000:1.947717,0.900000:1.995053,0.950000:2.022631,0.975000:2.036420;REF_CN=2;CI=0.05:1.82554,0.95:2.02263;RAW_CN=1.9')
     # expect(line["INFO"]).to eq("PASS")
@@ -59,11 +71,17 @@ RSpec.describe "Parser::ParseFactory", type: :model do
     # expect(line["INFO"]).to eq("PASS")
     expect(line["INFO"]["AF"]).to eq("0")
   end
-  
-  # it "parse_factory, Parse FILTER" do
-  #   line = {}
-  #   line["FILTER"] = parseFactory("FILTER", "PASS")
-  #   expect(line["FILTER"]).to eq("PASS")
-  # end
+
+  it "parse_factory, Parse FORMAT" do
+    line = {}
+    line["FORMAT"] = parseFormat("GT:GQ:CN", "./.:0:1.9")
+    expect(line["FORMAT"]["CN"]).to eq("1.9")
+  end
+
+  it "parse_factory, Parse FORMAT no :" do
+    line = {}
+    line["FORMAT"] = parseFormat("CN", "1.9")
+    # expect(line["FORMAT"]["CN"]).to eq("1.9")
+  end
 
 end
